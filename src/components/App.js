b71bc9447e88fd7c6e9cf0c722251ae5149762c2
Jean-Pierre Novak <jeanpierrenovak21@gmail.com>
2022-08-28 21:23:45 +0200

    Formated the integer to to show commas when number gets to big

--- src/components/App.js
+++ src/components/App.js
@@ -19,6 +19,13 @@ export const ACTIONS = {
 const reducer = (state, { type, payload }) => {
     switch(type) {
         case ACTIONS.ADD_DIGIT:
+            if(state.overwrite){
+                return {
+                    ...state,
+                    currentOperand: payload.digit,
+                    overwrite: false
+                }
+            }
             if(payload.digit === "0" && state.currentOperand === "0") {
                 return state;
             }
@@ -37,7 +44,7 @@ const reducer = (state, { type, payload }) => {
                 return {
                     ...state,
                     operation: payload.operation,
-                    
+
                 }
             }
             if(state.previousOperand == null) {
@@ -56,6 +63,36 @@ const reducer = (state, { type, payload }) => {
             }
         case ACTIONS.CLEAR:
             return {}
+        case ACTIONS.DELETE_DIGIT:
+            if(state.overwrite) {
+                return {
+                    ...state,
+                    overwrite: false,
+                    currentOperand: null
+                }
+            }
+            if(state.currentOperand == null) {
+                return state;
+            }
+            if(state.currentOperand.length === 1){
+                return {...state, currentOperand: null}
+            }
+            return {
+                ...state,
+                currentOperand: state.currentOperand.slice(0, -1)
+            }
+        case ACTIONS.EVALUATE:
+            if(state.operation === null || state.currentOperand == null || state.previousOperand == null){
+                return state
+            }
+            return {
+                ...state,
+                overwrite: true,
+                previousOperand: null,
+                operation: null,
+                currentOperand: evaluate(state),
+
+            }
     }
 }
 
@@ -80,6 +117,17 @@ const evaluate = ({ currentOperand, previousOperand, operation }) => {
     return computation.toString();
 }
 
+const INTEGER_FORMATER = new Intl.NumberFormat("en-us", {
+    maximumFractionDigits: 0,
+});
+
+const formatOperand = (operand) => {
+    if(operand == null) return;
+    const [integer, decimal] = operand.split('.');
+    if(decimal == null) return INTEGER_FORMATER.format(integer);
+    return `${INTEGER_FORMATER.format(integer)}.${decimal}`;
+}
+
 // Creating the App component
 const App = () => {
 
@@ -89,11 +137,11 @@ const App = () => {
         <div className="container">
             <div className="calculator-grid">
                 <div className="output">
-                    <div className="previous-operand">{previousOperand} {operation}</div>
-                    <div className="current-operand">{currentOperand}</div>
+                    <div className="previous-operand">{formatOperand(previousOperand)} {operation}</div>
+                    <div className="current-operand">{formatOperand(currentOperand)}</div>
                 </div>
                 <button className="span-two" onClick={() => dispatch({ type: ACTIONS.CLEAR})}>AC</button>
-                <button>DEL</button>
+                <button onClick={() => {dispatch({ type: ACTIONS.DELETE_DIGIT, })}}>DEL</button>
                 <OperationButton operation="÷" dispatch={dispatch} />
                 <DigitButton digit="1" dispatch={dispatch} />
                 <DigitButton digit="2" dispatch={dispatch} />
@@ -109,7 +157,7 @@ const App = () => {
                 <OperationButton operation="-" dispatch={dispatch} />
                 <DigitButton digit="." dispatch={dispatch} />
                 <DigitButton digit="0" dispatch={dispatch} />
-                <button className='span-two'>=</button>
+                <button className='span-two' onClick={() => {dispatch({type: ACTIONS.EVALUATE, })}}>=</button>
             </div>
         </div>
     );
