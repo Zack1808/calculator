e7d6fc772453d909e2f256a85f70a528f9bc423e
Jean-Pierre Novak <jeanpierrenovak21@gmail.com>
2022-08-28 20:57:08 +0200

    Created Reducer to perform the different calculations

--- src/components/App.js
+++ src/components/App.js
@@ -19,13 +19,65 @@ export const ACTIONS = {
 const reducer = (state, { type, payload }) => {
     switch(type) {
         case ACTIONS.ADD_DIGIT:
-            if(payload.digit === "0" && state.currentOperand === "0") return state;
-            if(payload.digit === "." && state.currentOperand.includes(".")) return state;
+            if(payload.digit === "0" && state.currentOperand === "0") {
+                return state;
+            }
+            if(payload.digit === "." && state.currentOperand.includes(".")) {
+                return state;
+            }
             return {
                 ...state,
                 currentOperand: `${state.currentOperand || ""}${payload.digit}`
             }
+        case ACTIONS.CHOOSE_OPERATION:
+            if(state.currentOperand == null && state.previousOperand == null) {
+                return state;
+            }
+            if(state.currentOperand == null) {
+                return {
+                    ...state,
+                    operation: payload.operation,
+                    
+                }
+            }
+            if(state.previousOperand == null) {
+                return {
+                    ...state,
+                    operation: payload.operation,
+                    previousOperand: state.currentOperand,
+                    currentOperand: null
+                }
+            }
+            return {
+                ...state,
+                previousOperand: evaluate(state),
+                operation: payload.operation,
+                currentOperand: null
+            }
+        case ACTIONS.CLEAR:
+            return {}
     }
+}
+
+const evaluate = ({ currentOperand, previousOperand, operation }) => {
+    const prev = parseFloat(previousOperand);
+    const current = parseFloat(currentOperand); 
+    if(isNaN(prev) || isNaN(current)) return "";
+    let computation = "";
+    switch(operation){
+        case "+":
+            computation = prev + current;
+            break;
+        case "-":
+            computation = prev - current;
+            break;
+        case "*":
+            computation = prev * current;
+            break;
+        case "÷":
+            computation = prev / current;
+    }
+    return computation.toString();
 }
 
 // Creating the App component
@@ -40,7 +92,7 @@ const App = () => {
                     <div className="previous-operand">{previousOperand} {operation}</div>
                     <div className="current-operand">{currentOperand}</div>
                 </div>
-                <button className="span-two">AC</button>
+                <button className="span-two" onClick={() => dispatch({ type: ACTIONS.CLEAR})}>AC</button>
                 <button>DEL</button>
                 <OperationButton operation="÷" dispatch={dispatch} />
                 <DigitButton digit="1" dispatch={dispatch} />
